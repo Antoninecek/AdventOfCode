783f58472432a9715869b7807195894f4c251f62
František Jukl <jukl@proconom.cz>
2023-12-05 22:40:47 +0100

    day4

---
+++ Day4/Part2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal static class Part2
+    {
+
+        public static int Run(string[] lines)
+        {
+
+
+            Dictionary<int, List<int>> winningCombos = new();
+            Dictionary<int, int> scretched = new();
+
+            foreach (string line in lines)
+            {
+                var cardNumber = int.Parse(Regex.Match(line, @"Card[ ]*([0-9]*):").Groups[1].Value);
+                string justNumbers = line.Substring(line.IndexOf(":") + 1);
+                var winning = justNumbers.Split('|').First().Split(" ").Where(x => x != string.Empty);
+                var my = justNumbers.Split('|').Last().Split(" ").Where(x => x != string.Empty);
+
+                var nWinning = my.Intersect(winning).Count();
+                winningCombos.Add(cardNumber, Enumerable.Range(cardNumber + 1, nWinning).ToList());
+            }
+
+            foreach (var cardNumber in winningCombos.Keys.Order())
+            {
+                var cardCombo = winningCombos[cardNumber];
+
+                foreach (var card in cardCombo)
+                {
+                    int n = 1;
+                    if (scretched.ContainsKey(cardNumber))
+                    {
+                        n += scretched[cardNumber];
+                    }
+
+                    if (!scretched.TryAdd(card, n))
+                    {
+                        scretched[card] += n;
+                    }
+                }
+            }
+            return scretched.Values.Sum() + lines.Length;
+        }
+    }
+}
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Day4;
+
 Console.WriteLine("Hello, World!");
+
+var lines = File.ReadAllLines("input4.txt");
+//lines = @"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+//Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+//Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+//Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+//Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+//Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".Split("\r\n");
+
+int sum = 0;
+
+foreach (string line in lines)
+{
+    string justNumbers = line.Substring(line.IndexOf(":") + 1);
+    var winning = justNumbers.Split('|').First().Split(" ").Where(x => x != string.Empty);
+    var my = justNumbers.Split('|').Last().Split(" ").Where(x => x != string.Empty);
+
+    sum += (int)Math.Pow(2, winning.Intersect(my).Count()-1);
+}
+
+Console.WriteLine(sum);
+Console.WriteLine(Part2.Run(lines));
