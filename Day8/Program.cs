1802d07d508ecf0aa9e5da1e33cee54b3c916ed6
František Jukl <jukl@proconom.cz>
2023-12-27 22:10:25 +0100

    day8 part2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -12,31 +12,38 @@ for (int i = 2; i < lines.Length; i++)
     maps.Add(lines[i].Substring(0, 3), new string[2] { lines[i].Substring(7, 3), lines[i].Substring(12, 3) });
 }
 
-char direction;
-string currentKey = "AAA";
+List<string> starts = maps.Keys.Where(x => x.EndsWith('A')).ToList();
 
-int nSteps = 0;
-
-while (currentKey != "ZZZ")
+foreach (var start in starts)
 {
-    foreach(char p in path)
+    char direction;
+    string currentKey = start;
+
+    int nSteps = 0;
+
+    while (!currentKey.EndsWith('Z'))
     {
-        direction = p;
-        nSteps++;
-        maps.TryGetValue(currentKey, out string[] paths);
-        if(direction == 'R')
+        foreach (char p in path)
         {
-            currentKey = paths[1];
-        }
-        if(direction == 'L')
-        {
-            currentKey = paths[0];
-        }
-        if(currentKey == "ZZZ")
-        {
-            break;
+            direction = p;
+            nSteps++;
+            maps.TryGetValue(currentKey, out string[] paths);
+            if (direction == 'R')
+            {
+                currentKey = paths[1];
+            }
+            if (direction == 'L')
+            {
+                currentKey = paths[0];
+            }
+            if (currentKey.EndsWith('Z'))
+            {
+                Console.WriteLine(nSteps);
+                break;
+            }
         }
     }
 }
 
-Console.WriteLine(nSteps);
+// najdi least common multiple ze vsech
+// https://www.calculatorsoup.com/calculators/math/lcm.php?input=13939+11309+20777+15517+17621+18673&data=none&action=solve
